e7270c0ab1086cef65e6718a62904a17f2f98964
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-13 23:16:13 +0900

    feat(validator): isNotInclude 함수 추가

--- src/validators.js
+++ src/validators.js
@@ -2,8 +2,6 @@ const PREFIX_ERROR = '[ERROR]';
 
 export const check = (message, validator) => {
     if (validator()) throw new Error(`${PREFIX_ERROR} ${message}`);
-
-    return false;
 };
 
 export const isMoreThan = (message, data, limit) => check(message, () => data > limit);
@@ -11,3 +9,5 @@ export const isMoreThan = (message, data, limit) => check(message, () => data > 
 export const isDuplicate = (message, list) => check(message, () => new Set(list).size !== list.length);
 
 export const isNotMatchRegex = (message, data, regex) => check(message, () => !regex.test(data));
+
+export const isNotInclude = (message, data, list) => check(message, () => !list.includes(data));
