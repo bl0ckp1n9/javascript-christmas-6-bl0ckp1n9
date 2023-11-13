9077727132ab5112549bcf272e003fc74e2a48cd
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-14 03:22:06 +0900

    feat(OutputView): 메뉴 출력 기능 문구 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,6 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
     printMenu(orders) {
+        Console.print('<주문 메뉴>');
         orders.forEach((order) => {
             Console.print(`${order.name} ${order.count}개`);
         });
