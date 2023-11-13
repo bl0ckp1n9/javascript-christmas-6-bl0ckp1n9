7e001674125d976d2dbce3c72e337136ad10fc92
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-13 18:19:24 +0900

    feat(InputView): 메뉴 입력

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,19 @@
-export default InputView = {
+import { Console } from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+    ASK_DATE:
+        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+    ASK_MENU:
+        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+};
+
+export const InputView = {
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+        const input = await Console.readLineAsync(MESSAGE.ASK_DATE);
+    },
+    async readMenu() {
+        const input = await Console.readLineAsync(MESSAGE.ASK_MENU);
+    },
+};
+
+export default InputView;
