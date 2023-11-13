4779ad3be9d5ccc60ab882ad9b2472ecb3dcdb4b
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-13 22:17:46 +0900

    refactor(validators): 필요없는 validator 제거

--- src/validators.js
+++ src/validators.js
@@ -1,43 +1,13 @@
-import { Console } from '@woowacourse/mission-utils';
-
 const PREFIX_ERROR = '[ERROR]';
 
 export const check = (message, validator) => {
-    try {
-        const isInvalid = validator();
-        if (isInvalid) throw new Error(`${PREFIX_ERROR} ${message}`);
+    if (validator()) throw new Error(`${PREFIX_ERROR} ${message}`);
 
-        return false;
-    } catch (e) {
-        Console.print(e.message);
-
-        return true;
-    }
+    return false;
 };
 
-export const isEmpty = (message, data) =>
-    check(message, () => data.length === 0);
+export const isMoreThan = (message, data, limit) => check(message, () => data > limit);
 
-export const isNotPositiveInteger = (message, data) =>
-    check(message, () => data < 1);
+export const isDuplicate = (message, list) => check(message, () => new Set(list).size !== list.length);
 
-export const isMoreThan = (message, data, limit) =>
-    check(message, () => data > limit);
-
-export const isLessThan = (message, data, limit) =>
-    check(message, () => data < limit);
-
-export const isNotIn = (message, data, list) =>
-    check(message, () => !list.includes(data));
-
-export const isDuplicate = (message, data, list) =>
-    check(message, () => list.includes(data));
-
-export const isOutOfRange = (message, data, range) =>
-    check(message, () => {
-        const { start, end } = range;
-
-        if (data < start || data > end) return true;
-
-        return false;
-    });
+export const isNotMatchRegex = (message, data, regex) => check(message, () => !regex.test(data));
