07751f15762c7c374925f54e65ddbd88f3d73706
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-14 03:21:26 +0900

    feat(OutputView): 메뉴 출력 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,11 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+    printMenu(orders) {
+        orders.forEach((order) => {
+            Console.print(`${order.name} ${order.count}개`);
+        });
+    },
+};
+
+export default OutputView;
