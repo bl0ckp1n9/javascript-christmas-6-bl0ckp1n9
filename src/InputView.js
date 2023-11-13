cd955217da0a486893387c19479fe2961182e18c
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-13 20:56:41 +0900

    feat(InputView): validation에 통과하지 못할 시 재입력 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,24 +1,32 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const MESSAGE = {
-    ASK_DATE:
-        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    ASK_MENU:
-        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+    ASK_DATE: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+    ASK_MENU: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
 };
 
 export const InputView = {
-    async readDate() {
-        const input = await this.readLineTrimAsync(MESSAGE.ASK_DATE);
+    async readDate(validate) {
+        const input = await this.readLineLoopAsync(MESSAGE.ASK_DATE, validate);
 
-        return input.trim();
+        return input;
     },
-    async readMenu() {
-        const input = await this.readLineTrimAsync(MESSAGE.ASK_MENU);
+    async readMenu(validate) {
+        const input = await this.readLineLoopAsync(MESSAGE.ASK_MENU, validate);
 
-        return input.trim();
+        return input;
     },
-
+    async readLineLoopAsync(message, validate) {
+        while (true) {
+            try {
+                const input = await this.readLineTrimAsync(message);
+                validate(input);
+                return input;
+            } catch (e) {
+                Console.print(e.message);
+            }
+        }
+    },
     async readLineTrimAsync(message) {
         const input = await Console.readLineAsync(message);
 
