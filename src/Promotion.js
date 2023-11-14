57bf173c3e1cd922ea64f7e7a966b51e2cd9a6e2
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-14 19:16:01 +0900

    feat(Prmotion): 주말, 평일 할인 기능

--- src/Promotion.js
+++ src/Promotion.js
@@ -1,8 +1,23 @@
+const PRMOTION_CATEGORY = {
+    CHRISTMAS: 'CHRISTMAS',
+    WEEKS: 'WEEKS',
+};
 export const ChristPromotion = {
     BASE_PRICE: 1_000,
     END_DATE: 25,
+    TYPE: PRMOTION_CATEGORY.CHRISTMAS,
     calcPromotionPrice(date) {
         if (date > this.END_DATE) return 0;
         return this.BASE_PRICE + this.BASE_PRICE * 0.1 * (date - 1);
+    },
+};
+
+export const WeeksPromotion = {
+    BASE_PRICE: 2_023,
+    END_DATE: 31,
+    TYPE: PRMOTION_CATEGORY.WEEKS,
+    calcPromotionPrice(orders, date) {
+        const menuCount = orders.reduce((acc, cur) => acc + Number(cur.count), 0);
+        return this.BASE_PRICE * menuCount;
     },
 };
