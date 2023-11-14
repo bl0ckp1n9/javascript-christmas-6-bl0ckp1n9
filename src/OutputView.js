0fe2186a74942e67ba9f30ac7c7bfa1c5e86870a
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-15 01:59:09 +0900

    feat(OutputView): 혜택 내역 출력 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { PROMOTION_CATEGORIES } from './constant.js';
 
 const OutputView = {
     printOrderMenus(orders) {
@@ -11,8 +12,16 @@ const OutputView = {
         Console.print('<할인 전 총주문 금액>');
         Console.print(`${totalPrice.toLocaleString()}원`);
     },
-    printEventBenefits(event) {
+    printBenefitDetails(promotions) {
         Console.print('<혜택 내역>');
+        const printData = [];
+        promotions.forEach((promotion) => {
+            const { NAME, PRODUCT, promotionBenefitPrice } = promotion;
+            const isApplied = promotionBenefitPrice > 0;
+            if (isApplied) printData.push(`${NAME} 할인: -${promotionBenefitPrice.toLocaleString()}원`);
+        });
+        if (printData.length === 0) printData.push('없음');
+        Console.print(printData.join('\n'));
     },
     printBadge(badge) {
         Console.print('<12월 이벤트 배지>');
@@ -20,7 +29,8 @@ const OutputView = {
     printTotalPriceWithDiscount(totalPriceWithDiscount) {
         Console.print('<할인 후 예상 결제 금액>');
     },
-    printGiveWayMenu(giftMenu) {
+    printGiveWayMenu(promotions) {
+        const giftMenu = promotions.find((promotion) => promotion.EVENT === PROMOTION_CATEGORIES.GIFT);
         const { PRODUCT, promotionBenefitPrice } = giftMenu;
         const isApplied = promotionBenefitPrice > 0;
 
