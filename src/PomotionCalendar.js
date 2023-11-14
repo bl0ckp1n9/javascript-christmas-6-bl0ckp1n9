06c676a3e9d101ae165e89aefb4c5ac9a6e08189
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-14 19:00:23 +0900

    feat(PromotionCalendar): PromotionCalender 생성

--- src/PomotionCalendar.js
+++ src/PomotionCalendar.js
@@ -6,16 +6,26 @@ class PromotionCalendar {
         const promotionDate = new Date(year, month - 1, 0);
         this.#endDate = new Date(year, month - 1, promotionDate.getDate());
         this.#startDate = new Date(year, month - 1, 1);
-        this.#memorizeDate();
+        this.#initalizeDate();
     }
 
-    #memorizeDate() {
+    #initalizeDate() {
         const start = this.#startDate.getDate();
         const end = this.#endDate.getDate();
         for (let date = start; date <= end; date += 1) {
-            this.#dateMemory.set(date);
+            this.#dateMemory.set(date, []);
         }
+    }
+
+    setEventToCalendar(date, event) {
+        const eventList = this.#dateMemory.get(date);
+        eventList.push(event);
+        this.#dateMemory.set(date, eventList);
+    }
+
+    getCalendarByDate(date) {
+        return this.#dateMemory.get(date);
     }
 }
 
-export default PromotionDate;
+export default PromotionCalendar;
