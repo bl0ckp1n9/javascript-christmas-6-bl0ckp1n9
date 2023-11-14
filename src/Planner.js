41183c443c2d732f359a45fddbdf040a8e9330de
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-15 01:32:19 +0900

    refactor(Planner): Order 클래스 분리로 인한 리팩토링

--- src/Planner.js
+++ src/Planner.js
@@ -1,113 +1,41 @@
-import { isDuplicate, isEveryInclude, isMoreThanLimit, isNotMatchRegex, isSomeNotInclude } from './validators.js';
+import { PROMOTION_CATEGORIES } from './constant.js';
 
-const ERROR_MESSAGE = Object.freeze({
-    INVALID_DATE: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
-    IS_INVALID_ORDER: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
-});
-
-const { INVALID_DATE, IS_INVALID_ORDER } = ERROR_MESSAGE;
 class Planner {
-    static validateOrderOnlyOneCategory(orders, menus, category) {
-        const { orderMenuNameList } = Planner.parseOrders(orders);
-        const menuNameListForCategory = Object.values(menus)
-            .filter((menu) => menu.TYPE === category)
-            .map((menu) => menu.NAME);
+    #order;
+    #calendar;
 
-        isEveryInclude(IS_INVALID_ORDER, orderMenuNameList, menuNameListForCategory);
+    constructor(order, calendar) {
+        this.#order = order;
+        this.#calendar = calendar;
     }
 
-    static validateIncludeMenu(orders, menus) {
-        const { orderMenuNameList } = Planner.parseOrders(orders);
-        const menuNameList = Object.values(menus).map((menu) => menu.NAME);
-
-        isSomeNotInclude(IS_INVALID_ORDER, orderMenuNameList, menuNameList);
-    }
-
-    static validateDuplicationOrder(orders) {
-        const { orderMenuNameList } = Planner.parseOrders(orders);
-
-        isDuplicate(IS_INVALID_ORDER, orderMenuNameList);
-    }
-
-    static validateLimitOrderCount(orders, limitOrderCount) {
-        const { totalOrderCount } = Planner.parseOrders(orders);
-
-        isMoreThanLimit(IS_INVALID_ORDER, totalOrderCount, limitOrderCount);
-    }
-
-    static validateOrderFormat(orders, regex) {
-        const { orderList } = Planner.parseOrders(orders);
-
-        orderList.forEach((order) => isNotMatchRegex(IS_INVALID_ORDER, order, regex));
-    }
-
-    static validateDayFormat(date, regex) {
-        isNotMatchRegex(INVALID_DATE, date, regex);
-    }
-
-    static parseOrders(orders) {
-        const orderList = orders.split(',').map((order) => order.trim());
-        const orderMenuNameList = orderList.map((order) => order.split('-')[0]);
-        const orderMenuCountList = orderList.map((order) => order.split('-')[1]);
-        const totalOrderCount = orderMenuCountList.reduce((acc, cur) => acc + Number(cur), 0);
-
-        return {
-            orderList,
-            orderMenuNameList,
-            orderMenuCountList,
-            totalOrderCount,
-        };
-    }
-
-    #orders = new Map();
-    #menus = new Map();
-    constructor(menus) {
-        this.#menus = new Map(Object.entries(menus));
-    }
-
-    setOrders(orders) {
-        const { orderMenuNameList, orderMenuCountList } = Planner.parseOrders(orders);
-        orderMenuNameList.forEach((menuName, index) => {
-            const category = this.#getCategoryByMenuName(menuName);
-            const key = this.#getKeyByName(menuName);
-
-            this.#orders.set(key, {
-                name: menuName,
-                count: orderMenuCountList[index],
-                category,
+    getPromotionsByOrderDate() {
+        const promotions = this.#calendar.getPromotionsByDate(this.#order.getOrderDate());
+        const activePromotions = [];
+        promotions.forEach((promotion) => {
+            const { CONFIG } = promotion;
+            const productInOrders = this.#order.getOrderMenuByCategory(CONFIG.PRODUCT);
+            activePromotions.push({
+                ...CONFIG,
+                promotionBenefitPrice: this.#calculatePromotions(promotion, productInOrders),
             });
         });
+        return activePromotions;
     }
-
-    getCategories() {
-        return Array.from(new Set(Array.from(this.#menus.values()).map((menu) => menu.TYPE)));
-    }
-    getOrders() {
-        return this.getCategories()
-            .map((categoryName) => this.getOrdersByCategory(categoryName))
-            .flat();
-    }
-    getOrdersByCategory(categoryName) {
-        return Array.from(this.#orders.values()).filter((order) => order.category === categoryName);
-    }
-    getPreDiscountTotalPrice() {
-        return Array.from(this.#orders.values()).reduce((acc, order) => {
-            const key = this.#getKeyByName(order.name);
-            const menu = this.#menus.get(key);
-            return acc + menu.PRICE * order.count;
-        }, 0);
-    }
-
-    validate(validators) {
-        validators.forEach((validator) => validator());
-    }
-
-    #getCategoryByMenuName(menuName) {
-        return Array.from(this.#menus.values()).find((menuMap) => menuMap.NAME === menuName).TYPE;
-    }
-
-    #getKeyByName(name) {
-        return Array.from(this.#menus.keys()).find((key) => this.#menus.get(key).NAME === name);
+    #calculatePromotions(promotion, orders) {
+        const promotionCategory = promotion.CONFIG.EVENT;
+        if (promotionCategory === PROMOTION_CATEGORIES.CHRISTMAS || promotionCategory === PROMOTION_CATEGORIES.SPECIAL)
+            return promotion.getPromotionPrice(this.#order.getOrderDate());
+        if (
+            promotionCategory === PROMOTION_CATEGORIES.WEEKENDS ||
+            promotionCategory === PROMOTION_CATEGORIES.WEEKDAYS
+        ) {
+            const productInOrders = this.#order.getOrderMenuByCategory(promotion.CONFIG.PRODUCT);
+            return promotion.getPromotionPrice(productInOrders);
+        }
+        if (promotionCategory === PROMOTION_CATEGORIES.GIFT)
+            return promotion.getPromotionPrice(this.#order.getTotalPrice());
+        return 0;
     }
 }
 
