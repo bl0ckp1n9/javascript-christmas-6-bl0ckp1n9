447767ec3dd11789a721c913f3aadecbdf7013c1
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-14 19:23:22 +0900

    feat(Prmotion): 특별, 증정 할인 기능

--- src/Promotion.js
+++ src/Promotion.js
@@ -1,23 +1,43 @@
 const PRMOTION_CATEGORY = {
     CHRISTMAS: 'CHRISTMAS',
     WEEKS: 'WEEKS',
+    SPECIAL: 'SPECIAL',
+    GIFT: 'GIFT',
 };
 export const ChristPromotion = {
-    BASE_PRICE: 1_000,
+    BENEFIT_PRICE: 1_000,
     END_DATE: 25,
     TYPE: PRMOTION_CATEGORY.CHRISTMAS,
-    calcPromotionPrice(date) {
+    getPromotionPrice(date) {
         if (date > this.END_DATE) return 0;
-        return this.BASE_PRICE + this.BASE_PRICE * 0.1 * (date - 1);
+        return this.BENEFIT_PRICE + this.BENEFIT_PRICE * 0.1 * (date - 1);
     },
 };
 
 export const WeeksPromotion = {
-    BASE_PRICE: 2_023,
-    END_DATE: 31,
+    BENEFIT_PRICE: 2_023,
     TYPE: PRMOTION_CATEGORY.WEEKS,
-    calcPromotionPrice(orders, date) {
+    getPromotionPrice(orders) {
         const menuCount = orders.reduce((acc, cur) => acc + Number(cur.count), 0);
-        return this.BASE_PRICE * menuCount;
+        return this.BENEFIT_PRICE * menuCount;
+    },
+};
+
+export const SpecialPromotion = {
+    BENEFIT_PRICE: 1_000,
+    END_DATE: 31,
+    TYPE: PRMOTION_CATEGORY.SPECIAL,
+    getPromotionPrice() {
+        return this.BENEFIT_PRICE;
+    },
+};
+
+export const GiftPromotion = {
+    BENEFIT_PRICE: 25_000,
+    GIFT_NAME: '샴페인',
+    END_DATE: 31,
+    TYPE: PRMOTION_CATEGORY.GIFT,
+    getPromotionPrice() {
+        return this.BENEFIT_PRICE;
     },
 };
