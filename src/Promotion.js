f93bc272613d03e69972aada4aac4f94769275a4
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-15 01:32:54 +0900

    refactor(Promotion): 불필요한 함수 제거

--- src/Promotion.js
+++ src/Promotion.js
@@ -3,8 +3,7 @@ import { PROMOTION_CONFIG } from './constant.js';
 export const ChristPromotion = {
     CONFIG: PROMOTION_CONFIG.CHRISTMAS,
     getPromotionPrice(date) {
-        if (date > 31) return 0;
-        return this.BENEFIT_PRICE + this.BENEFIT_PRICE * 0.1 * (date - 1);
+        return this.CONFIG.BENEFIT_PRICE + this.CONFIG.BENEFIT_PRICE * 0.1 * (date - 1);
     },
 };
 
@@ -12,30 +11,30 @@ export const WeekendsPromotion = {
     CONFIG: PROMOTION_CONFIG.WEEKENDS,
     getPromotionPrice(orders) {
         const menuCount = orders.reduce((acc, cur) => acc + Number(cur.count), 0);
-        return this.BENEFIT_PRICE * menuCount;
+        return this.CONFIG.BENEFIT_PRICE * menuCount;
     },
 };
 export const WeekdaysPromotion = {
     CONFIG: PROMOTION_CONFIG.WEEKDAYS,
     getPromotionPrice(orders) {
         const menuCount = orders.reduce((acc, cur) => acc + Number(cur.count), 0);
-        return this.BENEFIT_PRICE * menuCount;
+        return this.CONFIG.BENEFIT_PRICE * menuCount;
     },
 };
 
 export const SpecialPromotion = {
     CONFIG: PROMOTION_CONFIG.SPECIAL,
-    getPromotionPrice(date) {
-        if (!this.CONFIG.SPECIAL_DATES.includes(date)) return 0;
-        return this.BENEFIT_PRICE;
+    getPromotionPrice() {
+        return this.CONFIG.BENEFIT_PRICE;
     },
 };
 
 export const GiftPromotion = {
     CONFIG: PROMOTION_CONFIG.GIFT,
     getPromotionPrice(preDiscountAmount) {
-        if (preDiscountAmount < this.MINIMUM_PRICE) return 0;
-        return this.BENEFIT_PRICE;
+        if (preDiscountAmount < this.CONFIG.MINIMUM_PRICE) return 0;
+        return this.CONFIG.BENEFIT_PRICE;
     },
 };
+
 export const Promotions = [ChristPromotion, WeekendsPromotion, WeekdaysPromotion, SpecialPromotion, GiftPromotion];
